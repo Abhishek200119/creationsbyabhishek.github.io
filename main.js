71c5d80f3ba6331a1ebf2164bcfc3d82e38964aa
Abhishek Rajput <131696089+Abhishek200119@users.noreply.github.com>
2023-11-06 22:49:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".text" ,{
-    strings:["Front-end Developer","Web Developer","UI/UX Designer"],
+    strings:["Front-end Development","UI/UX Designing"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
